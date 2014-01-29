1a0b3570f77a0a3ae3d2b07ef7c915537b94e6d5
Serban Constantin <serban.constantin@gmail.com>
2014-01-30 01:39:00 +0200

    get list of logs in folder

--- parse.py
+++ parse.py
@@ -1,6 +1,8 @@
 #!env/bin/python
 import re
 import os
+from os import listdir
+from os.path import isfile, join
 
 normalline = re.compile('^\[[^\]]*(\d{2}):\d+:\d+\] <([^>]+)> (.*)$')
 actionline = re.compile('^\[[^\]]*(\d{2}):\d+:\d+\] \* (\S+) (.*)$')
@@ -8,3 +10,15 @@ serverline = re.compile('^\[[^\]]*(\d{2}):(\d+):\d+\] \*{3} (.+)$')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 logfolder = os.path.join(basedir, 'log')
+
+
+def get_files(logfolder):
+    files = [f for f in listdir(logfolder) if isfile(join(logfolder, f))]
+    return files
+
+
+def main():
+    get_files(logfolder)
+
+if __name__ == '__main__':
+    main()
