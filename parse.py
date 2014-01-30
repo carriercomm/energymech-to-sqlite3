17a0cc16fced6702a7bda8d4eb10754db8a67683
Serban Constantin <serban.constantin@gmail.com>
2014-01-31 01:11:08 +0200

    use threaded parser

--- parse.py
+++ parse.py
@@ -3,9 +3,10 @@ import re
 import os
 from os import listdir
 from os.path import isfile, join
-# from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing.dummy import Pool as ThreadPool
+# import timing
 
-normalline = re.compile('^\[[^\]]*(\d{2}:\d{2}:\d{2})\] <([^>]+)> (.*)$')
+chatline = re.compile('^\[[^\]]*(\d{2}:\d{2}:\d{2})\] <([^>]+)> (.*)$')
 actionline = re.compile('^\[[^\]]*(\d{2}:\d{2}:\d{2})\] \* (\S+) (.*)$')
 serverline = re.compile('^\[[^\]]*(\d{2}:\d{2}:\d{2})\] \*{3} (.+)$')
 
@@ -18,37 +19,46 @@ def get_files(logfolder):
     return files
 
 
-def get_normalline(log):
+def get_chatline(log):
     file = open(log)
     date = str(log)[-10:-4]
     results = []
     for line in file:
-        if normalline.match(line):
-            timestmp = normalline.search(line).group(1)
-            nick = normalline.search(line).group(2)
-            text = normalline.search(line).group(3)
+        if chatline.match(line):
+            timestmp = chatline.search(line).group(1)
+            nick = chatline.search(line).group(2)
+            text = chatline.search(line).group(3)
             say = {'date': date, 'timestamp': timestmp,
                    'nick': nick, 'text': text}
             results.append(say)
     return results
 
 
-# def parse_logs(files):
-#     """ try and get the files in a threaded fashion"""
-#     pool = ThreadPool(8)
-#     stuff = pool.map(get_normalline(files), files)
+def parse_logs(files):
+    """ form the date, timestamp, nick, text dictionary list
+    in an async fashion """
+    pool = ThreadPool(8)
+    chatdict = pool.map(get_chatline, files)
+    pool.close()
+    pool.join()
+    return chatdict
 
 
 def main():
-    files = get_files(logfolder)
-    print files
-    for file in files:
-        normallines = get_normalline(os.path.join(logfolder, file))
-    # for i, timestamp in enumerated(d['timestamp'] for d in normallines):
-    #    print i, timestamp
-    for a in normallines:
-        print str(a)
-    # parse_logs(files)
+    filesrel = get_files(logfolder)
+    # regular method
+    # for file in filesrel:
+    #     files = get_chatline(os.path.join(logfolder, file))
+    # for a in files:
+    #     print str(a)
+
+    # threaded method
+    files = []
+    for file in filesrel:
+        files.append(os.path.join(logfolder, file))
+    values = parse_logs(files)
+    for value in values:
+        print str(value)
 
 if __name__ == '__main__':
     main()
